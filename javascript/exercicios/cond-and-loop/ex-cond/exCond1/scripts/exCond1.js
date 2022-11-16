bda06475f06a6742a43b13fcf3f61abb95f04054
FrigusIgnis <araujo.silva.nicolas@gmail.com>
2022-11-16 20:02:49 +0000

    Added style conditions in script and color changes in the css, now all the three conditions should correspond to its respective daytime

--- javascript/exercicios/cond-and-loop/ex-cond/exCond1/scripts/exCond1.js
+++ javascript/exercicios/cond-and-loop/ex-cond/exCond1/scripts/exCond1.js
@@ -1,30 +1,34 @@
 function loading() {
     let bodySel = document.body;
+    let mainCont = document.querySelector('main#principal');
     let paragHora = document.querySelector('div#textHora');
     let imageHora = document.querySelector('div#imgHora');
     let dataAtual = new Date();
     let horaAtual = dataAtual.getHours();
-    paragHora.innerHTML = `<p>Agora são ${horaAtual} horas...</p>`;
 
-    if (horaAtual > 4 && horaAtual < 12) {
+    if (horaAtual >= 4 && horaAtual < 12) {
+        paragHora.innerHTML = `<p>Bom dia, agora são ${horaAtual} horas...</p>`;
         imageHora.style.background = "url('imagens/manha.jpg')";
         imageHora.style.backgroundPosition = "center center";
-        bodySel.style.backgroundColor = "#495b74";
-        document.querySelector('main#principal').style.backgroundColor = "#cadcf1";
+        bodySel.style.backgroundColor = "#92e1f5";
+        mainCont.style.backgroundColor = "#b8e5f0";
 
-    } else if (horaAtual > 12 && horaAtual < 18) {
-        imageHora.style.background = "url('imagens/noite.jpg')";
-        imageHora.style.backgroundPosition = "top center";
-        bodySel.style.backgroundColor = "#3a4047";
-        document.querySelector('main#principal').style.backgroundColor = "#fdefde";
-
-    } else {
-        imageHora.style.background = "url('imagens/noite.jpg')";
-        imageHora.style.backgroundPosition = "top center";
-        bodySel.style.backgroundColor = "#3a4047";
+    } else if (horaAtual >= 12 && horaAtual < 18) {
+        paragHora.innerHTML = `<p>Boa tarde, agora são ${horaAtual} horas...</p>`;
         imageHora.style.background = "url('imagens/tarde.jpg')";
         imageHora.style.backgroundPosition = "bottom center";
         bodySel.style.backgroundColor = "#495b74";
-        document.querySelector('main#principal').style.backgroundColor = "#fdefde";
+        mainCont.style.backgroundColor = "#ffd5b9";
+    } else {
+        if (horaAtual == 0) {
+            paragHora.innerHTML = `<p>Boa noite, agora é meia-noite...</p>`;
+        } else {
+            paragHora.innerHTML = `<p>Boa noite, agora são ${horaAtual} horas...</p>`;
+        }
+
+        imageHora.style.background = "url('imagens/noite.jpg')";
+        imageHora.style.backgroundPosition = "top center";
+        bodySel.style.backgroundColor = "#3a4047";
+        mainCont.style.backgroundColor = "#d4d4d4";
     }
 }
